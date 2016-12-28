8ff2690d350e2740659fed4b86fe4b4830d75d7a
Steven Zhang <steven.zhang031@gmail.com>
2016-12-28 01:02:54 -0800

    add category endpoint

---
+++ src/main/groovy/org/usales/api/cm/CategoryEndpoint.groovy
@@ -0,0 +1,16 @@
+package org.usales.api.cm
+
+import groovy.util.logging.Slf4j
+import ratpack.groovy.handling.GroovyChainAction
+
+/**
+ * Created by hzhang on 12/28/2016.
+ */
+@Slf4j
+class CategoryEndpoint extends GroovyChainAction {
+
+    @Override
+    void execute() throws Exception {
+
+    }
+}
