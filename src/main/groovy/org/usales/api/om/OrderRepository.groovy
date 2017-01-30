6c03b04c8ddc99371109e3b6232b6f4f7eb09b49
steven zhang <steven.zhang031@gmail.com>
2017-01-29 22:23:30 -0800

    add some order repo

--- src/main/groovy/org/usales/api/om/OrderRepository.groovy
+++ src/main/groovy/org/usales/api/om/OrderRepository.groovy
@@ -1,6 +1,7 @@
 package org.usales.api.om
 
 import groovy.sql.Sql
+import ratpack.exec.Promise
 
 import javax.inject.Inject
 
@@ -13,6 +14,30 @@ class OrderRepository {
     @Inject
     OrderRepository(Sql sql) {
         this.sql = sql
+    }
+
+    Promise<List<Order>> get(Long id) {
+
+    }
+
+    Promise<List<Order>> find(String name, Integer status) {
+
+    }
+
+    Promise<Order> create(Order order) {
+
+    }
+
+    Promise<Order> save(Order order) {
+
+    }
+
+    Promise<OrderLine> getLine(Long id) {
+
+    }
+
+    Promise<OrderLine> add(Order order, OrderLine) {
+
     }
 
 }
