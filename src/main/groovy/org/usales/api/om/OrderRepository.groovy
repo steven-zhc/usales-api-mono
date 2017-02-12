9e8ffc45fd7660aefc0a8e172f0b2006785302c8
steven zhang <steven.zhang031@gmail.com>
2017-02-12 00:46:30 -0800

    update async arch

--- src/main/groovy/org/usales/api/om/OrderRepository.groovy
+++ src/main/groovy/org/usales/api/om/OrderRepository.groovy
@@ -32,27 +32,25 @@ class OrderRepository {
 INSERT INTO `order` (`date_created`, `note`, `status`, `profit`, `total`)
 VALUES (NOW(), $order.note, $order.status, $order.profit, $order.total)
 """, ['id'])
-        }.flatMap {
+        }.then {
             order.oid = it[0][0] as long
 
-            Operation.of {
-                order.lines.each { l ->
-                    Blocking.get {
-                        sql.executeInsert("""
+            order.lines.each { l ->
+                Blocking.get {
+                    sql.executeInsert("""
 INSERT INTO `order_line_t` (`date_created`, `line_profit`, `line_total`, `model`, `note`, `order_id`, `product_id`, 
     `quantity`, `s_discount`, `s_price`, `s_shipping`, `s_tax`, `s_total`, `p_discount`, `p_price`, `p_shipping`, `p_tax`, `p_total`) 
 VALUES (NOW(), $l.profit, $l.total, $l.model, $l.note, $order.oid, $l.pid, $l.q, 
 $l.sell.d, $l.sell.p, $l.sell.s, $l.sell.t, $l.sell.total,
 $l.purchase.d, $l.purchase.p, $l.purchase.s, $l.purchase.t, $l.purchase.total)
 """, ['id'])
-                    }.then {
-                        l.lid = it[0][0] as long
-                        l
-                    }
                 }
-            }.map {
-                order
+                .then { l.lid = it[0][0] as long }
             }
+
+            Operation.of().then()
+
+            order
         }
 
     }
